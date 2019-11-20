cddc40a853924decb5ed9adf7ae646f0c7ea36fb
Taras <atbeptka@gmail.com>
2019-11-20 23:26:50 +0300

    Добавлена возможность загрузки html страницы с диска и интернета с последующим выводом ее заголовка через XPath.

---
+++ RatesParsingConsole/RatesParsingConsole/GettingHtml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using HtmlAgilityPack;
+
+
+namespace RatesParsingConsole
+{
+    /// <summary>
+    /// Работа с входными данными.
+    /// </summary>
+    class GettingHtml
+    {
+        /// <summary>
+        /// Настройки для получения html документа.
+        /// </summary>
+        public ParseSettings Settings { get; set; }
+
+        public GettingHtml() { }
+
+        /// <summary>
+        /// Установить настройки получения .html документа.
+        /// </summary>
+        /// <param name="parse">Содержит настройки для парсинга.</param>
+        public GettingHtml(ParseSettings parse)
+        {
+            Settings = parse;
+        }
+
+        /// <summary>
+        /// Получить html документ из файла, указанного в свойстве HttpFileName в настройках парсинга.
+        /// </summary>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromFile()
+        {
+            return getHtmlDocumentFromFile(Settings.HttpFileName);
+        }
+
+        /// <summary>
+        /// Получить html документ из файла.
+        /// </summary>
+        /// <param name="FilePath">Путь к файлу.</param>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromFile(string FileName)
+        {
+            return getHtmlDocumentFromFile(FileName);
+        }
+
+        /// <summary>
+        /// Получить html документ из интернета.
+        /// </summary>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromWeb()
+        {
+            return getHtmlDocumentFromWeb(Settings.HttpUrl);
+        }
+
+        /// <summary>
+        /// Получить html документ из интернета.
+        /// </summary>
+        /// <param name="URL">Адрес получаемой страницы.</param>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromWeb(string URL)
+        {
+            return getHtmlDocumentFromWeb(URL);
+        }
+
+        /// <summary>
+        /// Метод для реализации перегрузки получения документа из файла.
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns></returns>
+        private HtmlDocument getHtmlDocumentFromFile(string FileName)
+        {
+            var document = new HtmlDocument();
+            try
+            {
+                document.Load(Settings.HttpFileName);
+            }
+            catch
+            {
+                Console.WriteLine($"Файл \"...\\{Settings.HttpFileName}\" не найден.\n");
+                return null;
+            }
+            Console.WriteLine($"Файл \"...\\{Settings.HttpFileName}\" успешно открыт.\n");
+            return document;
+        }
+        /// <summary>
+        /// Метод для реализации перегрузки получения документа из интернета.
+        /// </summary>
+        /// <param name="URL">Адрес страницы.</param>
+        /// <returns></returns>
+        private HtmlDocument getHtmlDocumentFromWeb(string URL)
+        {
+            var web = new HtmlWeb();
+            var document = new HtmlDocument();
+            try
+            {
+                document = web.Load(Settings.HttpUrl);
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine($"Не верный формат URL ({URL}).\n");
+                return null;
+            }
+            catch (System.Net.WebException)
+            {
+                Console.WriteLine($"Запрашиваемая страница не найдена ({URL}).\n");
+                return null;
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine($"Запрашиваемая страница не задана.\n");
+                return null;
+            }
+            
+            Console.WriteLine($"Страница {URL} успешно загружена.\n");
+            return document;
+        }
+    }
+}
--- RatesParsingConsole/RatesParsingConsole/Program.cs
+++ RatesParsingConsole/RatesParsingConsole/Program.cs
@@ -5,19 +5,62 @@ namespace RatesParsingConsole
 {
     class Program
     {
+        // Загрузить html файл с жесткого диска и вывести в консоль заголовок страницы. То же самое проделать со страницей из интернета.
         static void Main(string[] args)
         {
-            // Получить html документ из файла.
-            var filePath = @"Other\Sites\myfin.by\myfin.by.html1";
-            var inputData = new InputData();
-            HtmlDocument HtmlDoc = inputData.GetHtmlDocumentFromFile(filePath);
+            // Установить параметры для парсинга (имя файла и адрес страницы).
+            var parseSettings = new ParseSettings
+            {
+                HttpFileName = @"Other\Sites\Georgia\National Bank Of Georgia.html",
+                HttpUrl = "https://www.nbg.gov.ge/index.php?m=582&lng=eng"
+            };
 
-            // При успешном открытии вывести содержимое документа согласно .
-            if (HtmlDoc != null)
-                Console.WriteLine(HtmlDoc.Text);
-            else
-                Console.WriteLine(inputData.ErrorText);
-            Console.ReadLine();
+            // Получить инструкции JSON. 
+            // Пока не пилил, просто пропишем XPath.
+            string xPath = "/html/head/title";
+
+            // Инструмент для получения html страниц.
+            var gettingHtml = new GettingHtml(parseSettings);
+
+            // Получить html документ из файла.
+            Console.WriteLine("Получить документ из файла:");
+            HtmlDocument HtmlDocFile = gettingHtml.GetHtmlDocumentFromFile();
+            // При успешном открытии вывести заголовок документа.            
+            if (HtmlDocFile != null)
+            {
+                Console.WriteLine("Заголовок страницы из файла:");
+                Console.WriteLine(GetHtmlHead(HtmlDocFile, xPath));
+            }
+            Console.WriteLine();
+            
+            
+            // Получить html документ из интернета.      
+            Console.WriteLine("Нажмите любую клавишу для загрузки страницы из интернета.");
+            Console.ReadKey();
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Получить документ из интернета:");
+            HtmlDocument HtmlDocWeb = gettingHtml.GetHtmlDocumentFromWeb();
+            // При успешном открытии вывести заголовок документа.            
+            if (HtmlDocWeb != null)
+            {
+                Console.WriteLine("Заголовок страницы из интернета:");
+                Console.WriteLine(GetHtmlHead(HtmlDocWeb, xPath));
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Получить заголовок страницы.
+        /// </summary>
+        /// <param name="htmlDocument">Объект html документа.</param>
+        /// <param name="xPath">Текст внутри тега по адресу XPath.</param>
+        /// <returns></returns>
+        private static string GetHtmlHead(HtmlDocument htmlDocument, string xPath)
+        {
+            //Console.WriteLine();
+            string HtmlHead = htmlDocument.DocumentNode.SelectSingleNode(xPath).InnerText;
+            Console.WriteLine();
+            return HtmlHead;
         }
     }
 }
