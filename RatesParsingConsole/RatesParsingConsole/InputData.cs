90ef1ef8627c6315e65a0bed4ae2039d4fc39bb2
Taras <atbeptka@gmail.com>
2019-11-20 20:56:22 +0300

    До переноса try-catch в класс Program

---
+++ RatesParsingConsole/RatesParsingConsole/InputData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using HtmlAgilityPack;
+
+
+namespace RatesParsingConsole
+{
+    /// <summary>
+    /// Работа с входными данными.
+    /// </summary>
+    class InputData
+    {
+        /// <summary>
+        /// Текст ошибки. 
+        /// Чето какуюто хуйню похоже придумал. В класс Program пихать не хочется проверку исключений, хочется чтобы здесь все обрабатывалось.
+        /// </summary>
+        public string ErrorText { get; private set; }
+
+        /// <summary>
+        /// Получить html документ из файла.
+        /// </summary>
+        /// <param name="FilePath">Путь к файлу.</param>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromFile(string FilePath)
+        {
+            var document = new HtmlDocument();
+            try
+            {
+                document.Load(FilePath);
+            }
+            // Если файл не найден, возвращаем null.
+            catch (FileNotFoundException)
+            {
+                ErrorText = $"Файл \"...\\{FilePath}\" не найден.";
+                return null;
+            }
+            
+            return document;
+        }
+
+        /// <summary>
+        /// Получить html документ из сети.
+        /// </summary>
+        /// <param name="URL">Адрес получаемой страницы.</param>
+        /// <returns></returns>
+        public HtmlDocument GetHtmlDocumentFromWeb(string URL)
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(URL);
+            return document;
+        }
+    }
+}
--- RatesParsingConsole/RatesParsingConsole/Program.cs
+++ RatesParsingConsole/RatesParsingConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HtmlAgilityPack;
 
 namespace RatesParsingConsole
 {
@@ -6,7 +7,17 @@ namespace RatesParsingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test commit");
+            // Получить html документ из файла.
+            var filePath = @"Other\Sites\myfin.by\myfin.by.html1";
+            var inputData = new InputData();
+            HtmlDocument HtmlDoc = inputData.GetHtmlDocumentFromFile(filePath);
+
+            // При успешном открытии вывести содержимое документа согласно .
+            if (HtmlDoc != null)
+                Console.WriteLine(HtmlDoc.Text);
+            else
+                Console.WriteLine(inputData.ErrorText);
+            Console.ReadLine();
         }
     }
 }
